27e29355d1ac356576e149d3246eda12dce27dd3
Ion Zaiat <izaiat@pentalog.com>
2018-04-24 22:51:21 +0300

    from text input into textarea

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
 </div>
 <div class='form-group'>
     {!! Form::label('content', 'Post content:') !!}
-    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+    {!! Form::textArea('content', null, ['class' => 'form-control']) !!}
 </div>
 <div class='form-group'>
     {!! Form::submit($submitButtonText, ['class' => 'btn btn-lg btn-success form-control']) !!}
