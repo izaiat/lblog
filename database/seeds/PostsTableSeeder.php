d01268853cf9fa6aa9121ebc045663fa4aa813cf
Ion Zaiat <izaiat@pentalog.com>
2018-04-24 20:08:43 +0300

    Add Post Seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,10) as $index) {
+            DB::table('posts')->insert([
+                'title' => $faker->name,
+                'content' => $faker->text(50),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
+    }
+}
